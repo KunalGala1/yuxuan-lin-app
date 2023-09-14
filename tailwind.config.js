64ae30698fdf12bf35dd3669846fd1ca72f79688
Kunal Gala <kunal.gala16@gmail.com>
2023-09-14 15:18:19 -0400

    dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
       serif: ["Playfair Display", "Times", "serif"],
+      elsie: ["Elsie", "cursive"],
+    },
+    screens: {
+      xxs: "360px",
+      xs: "475px",
     },
     extend: {},
   },
