ff30a21f085fa86477129c06cbe391804cb4b952
Kunal Gala <kunal.gala16@gmail.com>
2023-09-22 14:43:09 -0400

    dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ['./views/**/*.ejs'],
   theme: {
     fontFamily: {
-      sans: ["Inter", "sans-serif"],
-      serif: ["Playfair Display", "Times", "serif"],
-      elsie: ["Elsie", "cursive"],
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Playfair Display', 'Times', 'serif'],
+      elsie: ['Elsie', 'cursive'],
     },
     screens: {
-      xxs: "360px",
-      xs: "475px",
+      xxs: '360px',
+      xs: '540px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     extend: {},
   },
